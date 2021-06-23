114b3fde0cbc0881f3ddfc98206f04c8f03b472d
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2021-06-22 23:17:45 -0500

    audio analyzer

---
+++ analyze_video/main.py
@@ -0,0 +1,10 @@
+import subprocess
+import time
+
+start = time.time()
+#subprocess.run(['docker', 'exec', '-it', 'extract_audio', 'python', 'analyze/src/extract_audio.py'])
+subprocess.run(['docker', 'exec', '-it', 'separate_audio', 'python', 'analyze/src/separate_audio.py'])
+subprocess.run(['docker', 'exec', '-it', 'transcript_audio', 'python', 'analyze/src/transcript.py'])
+end = time.time()
+
+print(end - start)
